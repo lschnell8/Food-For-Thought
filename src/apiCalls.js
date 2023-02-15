29a9c7bb9b4f0bdbeefb094fa3c83a56581e3745
lschnell8 <lschnell8@gmail.com>
2023-02-15 13:09:54 -0700

    Does this work

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,3 +1,3 @@
 export const fetchApi = () => {
-  return fetch(`https://mediterranean-recipes-api.vercel.app/v1/recipes`)
+  return fetch(`https://mediterranean-recipes-1tmijpbeg-lschnell8.vercel.app/v1/recipes`)
 }
